217e50b0bbe3a0b0d4cc4cae623a78b7e8a24a46
Sergei Zherdev <zherdev.se@gmail.com>
2022-02-14 01:06:48 +0500

    11.02.2022

--- footer.php
+++ footer.php
@@ -9,6 +9,29 @@
     <title>Document</title>
 </head>
 <body>
+<?
+$glas = ['H','а','у','о','и','э','ы','А','У','О','И','Э','Ы'];
+$site1 =file_get_contents("homework.php",);
+$site = mb_str_split($site1);
+$n=0;
+foreach ($site as $value){
+    if(in_array($value,$glas))
+    {$n ++;
+    }
+}
+echo "Количество гласных на странице: $n", '<br>';
+echo "Количество слов на странице:", str_word_count($site1);
+echo '<br>';
+$date1 = "24.06.1987";
+$date2=date("d.m.Y");
+echo "Дата рождения:", $date1,'<br>';
+echo "Текущая дата:", $date2,'<br>';
+$date3 = strtotime($date1);
+$date4 = strtotime($date2);
+$date5 = $date4-$date3;
+echo "Количество дней между датами:", floor($date5 / (60 * 60 * 24));
+?>
+
     <footer class="footer">
         <h3>Контакты:</h3>
         <h5>+7-906-871-88-00</h5>
