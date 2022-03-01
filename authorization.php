412f4d1cf64d2e5b8f97ee86b60a2a2b7970e5f0
Sergei Zherdev <zherdev.se@gmail.com>
2022-03-02 01:21:35 +0500

    28.02.2022

--- authorization.php
+++ authorization.php
@@ -1,9 +1,37 @@
 <?php
 session_start();
-
+$hostname= 'localhost';
+$username= 'Serg';
+$password= '1';
+$dbname= 'Auth';
+$db_con=mysqli_connect($hostname, $username, $password,$dbname);
+mysqli_set_charset($db_con,"UTF-8");
+//mysqli_query($db_con, "CREATE DATABASE $dbname");
+/*mysqli_query($db_con, "CREATE Table Users(
+    Id_user int NOT NULL AUTO_INCREMENT,
+    Login varchar (40),
+    Password varchar (40),
+    PRIMARY KEY(Id_user)
+)");*/
 $logins=['Сергей','Admin'];
 $passwords=['a01610228fe998f515a72dd730294d87','e3afed0047b08059d0fada10f400c1e5'];//1212 , Admin
-if(!empty($_POST)) {
+/*$user1= "INSERT INTO `Users`(`ID_user`, `Login`, `Password`) VALUES (NULL,'$logins[0]','$passwords[0]')";
+$user2= "INSERT INTO `Users`(`ID_user`, `Login`, `Password`) VALUES (NULL,'$logins[1]','$passwords[1]')";
+mysqli_query($db_con,$user1);
+mysqli_query($db_con,$user2);*/
+if ( !empty($_POST['password']) and !empty($_POST['login']) ) {
+$login = $_POST['login'];
+$password = $_POST['password'];
+    $query = "SELECT `Login`, `Password` FROM `Users` WHERE `Login` = '$login'";
+    $result = mysqli_query($db_con, $query);
+    $user = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    if ($user['0']['Password'] === md5($_POST['password'])) {
+        echo $_POST['login'], ", добро пожаловать!";
+    } else {
+        echo "Вы ввели неправильный логин/пароль";
+    }
+}
+/*if(!empty($_POST)) {
     $login = $_POST['login'];
     $password = md5($_POST['password']);
     $_POST['password'] = md5($_POST['password']);
@@ -52,12 +80,10 @@ else
             echo $_COOKIE['login'], $value;
         }
     }
-}
+}*/
 
 //session_destroy()
 ?>
-
-
 <!doctype html>
 <html lang="en">
 <head>
---
+++ users.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Мар 01 2022 г., 23:18
+-- Версия сервера: 5.6.51-log
+-- Версия PHP: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `auth`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `Id_user` int(11) NOT NULL,
+  `Login` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Password` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`Id_user`, `Login`, `Password`) VALUES
+(1, 'Сергей', 'a01610228fe998f515a72dd730294d87'),
+(2, 'Admin', 'e3afed0047b08059d0fada10f400c1e5');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Id_user`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `Id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
